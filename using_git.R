e85c0bffbe9ec83cd56bc9f750c419b0fcfa9198
Hank Stevens <hank.stevens@miamioh.edu>
2020-08-30 14:28:12 -0400

    all sun

---
+++ using_git.R
@@ -0,0 +1,11 @@
+library(devtools) 
+install_github("HankStevens/primer", force=TRUE)
+library(primer)
+
+# to commit everything
+# > git add -A && git commit -m 'staging all files'
+
+
+# git add -A to add all files new files, changes and removed files.
+# git commit -m  "Your message" to save the changes done in the files.
+# git push -u origin master to send your committed changes to a remote repository, where the local branch is named master to the remote named origin
